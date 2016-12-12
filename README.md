37cac7e742a6461af190f3d7bc810e458d3e7e20
Samuel <Samuel_ipad2@hotmail.com>
2016-12-12 11:48:08 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Make10calc
-http://make10.bamuel.com/
+#A php calculator for solving the MAKE 10 puzzle
+-[http://make10.bamuel.com/](http://make10.bamuel.com/) an online version available
+##Introduction
+- This was a challenge that I had todo mentally 6,6,7,8. After spending 2 days on it. I manage to make ten at 2am.
+- The point of the challenge was to make "10" using + - * / (). 
+##How the code works
+- index.php loads up the  < form >  and it goes through calculation.php then if it can't calculate it rearranges the numbers and redirects to calculation2.php and etc... till calculation24.php (max possible ways to rearrange a,b,c,d). If it is impossible todo the page will redirect to calculation24.php displaying a final output message.
+##Todo
+- Make an option todo Make 24. 
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elizabeth
+ * Date: 11/12/2016
+ * Time: 2:54 PM
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Make 10 Calculator</title>
+    <link href="" rel="stylesheet" type="text/css">
+</head>
+<body>
+<pre>Insert 4 digits to make</pre>
+<form action="calculation.php" method="get">
+    <input type="number" max="9" min="0" placeholder="1" name="a">
+    <input type="number" max="9" min="0" placeholder="2" name="b">
+    <input type="number" max="9" min="0" placeholder="3" name="c">
+    <input type="number" max="9" min="0" placeholder="4" name="d">
+    <!--<input type="number" value="10" name="value" disabled style="position: relative; top: 10px; left: 180px; max-width: 30px"><!-- for future-->-->
+    <br>
+    <br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
