21f7a0aeafc5954002e6f0effb597aa4881b74a0
Samuel Levin <me@bamuel.com>
2025-06-13 11:33:23 +1000

    Update manifest link and add web app manifest

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ function Calculate($ten, $a, $b, $c, $d) {
     <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
-    <link rel="manifest" href="/favicons/site.webmanifest">
+    <link rel="manifest" href="/favicons/webmanifest.json">
     <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000">
     <link rel="shortcut icon" href="/favicons/favicon.ico">
     <meta name="apple-mobile-web-app-title" content="Make 10">
